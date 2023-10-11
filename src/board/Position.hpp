72c8ce14a48040bb80f89d89b988ffacc0c25f3a
Seweryn <sevveryn.czykinowski@gmail.com>
2023-10-11 17:26:39 +0200

    Fixed missing header files

---
+++ src/board/Position.hpp
@@ -0,0 +1,27 @@
+#ifndef CHESS_CPP_POSITION_H
+#define CHESS_CPP_POSITION_H
+
+#include <stdexcept>
+
+struct Position {
+    enum {
+        MAX_POSITION = 8
+    };
+    int x;
+    int y;
+
+    Position(int x, int y);
+
+    static bool isPositionValid(unsigned int x, unsigned int y);
+
+    bool operator==(const Position &other) const;
+
+    bool operator!=(const Position &other) const;
+
+    bool operator<(const Position &other) const;
+
+    ~Position() = default;
+};
+
+
+#endif //CHESS_CPP_POSITION_H
---
+++ src/pieces/Piece.hpp
@@ -0,0 +1,39 @@
+#ifndef CHESS_CPP_PIECE_H
+#define CHESS_CPP_PIECE_H
+
+#include "../board/Position.hpp"
+
+enum class PieceColor {
+    BLACK, WHITE
+};
+
+enum class PieceType {
+    PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, NONE
+};
+
+class Piece {
+private:
+    PieceColor color;
+    PieceType type;
+    unsigned int value;
+    unsigned int moveCount;
+protected:
+    Piece(PieceColor color, PieceType type, unsigned int value);
+
+public:
+    [[nodiscard]] PieceColor getColor() const;
+
+    [[nodiscard]] PieceType getType() const;
+
+    [[nodiscard]] unsigned int getValue() const;
+
+    [[nodiscard]] unsigned int getMoveCount() const;
+
+    [[nodiscard]] bool isFirstMove() const;
+
+    void incrementMoveCount();
+
+    virtual ~Piece() = default;
+};
+
+#endif //CHESS_CPP_PIECE_H
